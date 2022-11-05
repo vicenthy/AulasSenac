4b29776568e6da5073e95e351876667182071974
Atila <atila.sistemas@gmail.com>
2022-11-04 21:31:09 -0300

    finalização da aula 02

--- aula_02/index.js
+++ aula_02/index.js
@@ -1,11 +1,38 @@
+const valorResultado = 
+function () { return document.getElementById('resultado').innerText;} 
+
 
 // funções de calculo
-function resultado(){}
-function calculo(operacao){}
-function limpar(){}
+function resultado(){
+    if(valorResultado()){
+        if(valorResultado().includes('0') && valorResultado().includes('/')){
+            alert('divisão por zero');
+            limpar();
+            return;
+        }if(Array.from(valorResultado()).every(a => isNaN(a)) ){
+            alert('nenhuma número encontrado');
+            limpar();
+            return;
+        }
+        document.getElementById('resultado').innerText = eval(valorResultado());
+        console.log('resultado: ' + valorResultado());     
+    }else{
+        alert('nenhum valor digitado')
+    }
+}
+
+function calculo(operacao){
+    document.getElementById('resultado').innerHTML = valorResultado() + operacao;
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = "";
+}
 
 //numeros
-function numero(numero){}
+function numero(numero){
+    document.getElementById('resultado').innerHTML = valorResultado() + numero;
+}
 
 
 
